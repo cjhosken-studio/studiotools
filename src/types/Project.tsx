30df2e1925959cecdae6cd2fee75c7a8364bd33d
Christopher Hosken <hoskenchristopher@gmail.com>
2025-08-20 00:37:49 +0100

    folders and task areas

--- src/types/Project.tsx
+++ src/types/Project.tsx
@@ -4,48 +4,60 @@ import { join } from "@tauri-apps/api/path";
 import { mkdir, create } from "@tauri-apps/plugin-fs";
 
 export default class Project {
-    name: string = "";
-    path: string = "";
+  name: string = "";
+  path: string = "";
 
-    constructor(name: string, path: string) {
-        this.name = name;
-        this.path = path;
-    }
+  constructor(name: string, path: string) {
+    this.name = name;
+    this.path = path;
+  }
 
-    setName(name: string) {
-        this.name = name
-    }
+  setName(name: string) {
+    this.name = name
+  }
 
-    setPath(path: string) {
-        this.path = path
-    }
+  setPath(path: string) {
+    this.path = path
+  }
 }
 
 const defaultProjectStructure = [
-  "config",
-  "edit",
-  "asset",
+  "sandbox",
+  "build",
   "sequence",
+  "editorial"
 ]
 
-export async function createProject(name: string, path: string) : Promise<Project> {
-    
-    await mkdir(path);
+export async function createProject(name: string, path: string): Promise<Project> {
 
-    for (const folder of defaultProjectStructure) {
-      const folderPath = await join(path, folder);
-      await mkdir(folderPath);
-    }
+  await mkdir(path);
 
-    const configPath = await join(path, "project.yaml");
-
-    const configFile = await create(configPath);
-    await configFile.write(new TextEncoder().encode(
-         `name: ${name}\npath: ${path}\n`
+  for (const folder of defaultProjectStructure) {
+    const folderPath = await join(path, folder);
+    await mkdir(folderPath);
+    const folderConfigPath = await join(folderPath, "folder.yaml");
+    const folderConfigFile = await create(folderConfigPath);
+    await folderConfigFile.write(new TextEncoder().encode(
+      `type: folder\n`
     ));
-    await configFile.close();
+    await folderConfigFile.close();
+  }
 
-    return new Project(name, path);
+  const configPath = await join(path, "project.yaml");
+  const folderConfigPath = await join(path, "folder.yaml");
+  const folderConfigFile = await create(folderConfigPath);
+  await folderConfigFile.write(new TextEncoder().encode(
+    `type: project\n`
+  ));
+  await folderConfigFile.close();
+
+  const configFile = await create(configPath);
+  await configFile.write(new TextEncoder().encode(
+    `name: ${name}\npath: ${path}\n`
+  ));
+  await configFile.close();
+
+  return new Project(name, path);
 }
 
 export async function loadHomeProjects(): Promise<Project[]> {
