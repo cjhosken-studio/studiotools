de275fe4b5b57fb5976fd4ef56574710b31d4621
Christopher Hosken <hoskenchristopher@gmail.com>
2025-12-26 10:13:21 +0700

    prep for merge

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/.venv/bin/activate"
 
-PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/plugins/usd/installs/OpenUSD/lib/python"
-PATH="$PATH:$SCRIPT_DIR/plugins/usd/installs/OpenUSD/bin"
+PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/installs/lib/python"
+PATH="$PATH:$SCRIPT_DIR/usd/installs/bin"
 
 python "$SCRIPT_DIR/src/main.py"
