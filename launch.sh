1515deacdcf9559ba0fc3bd173535c64ec5de5ba
Christopher Hosken <hoskenchristopher@gmail.com>
2025-12-27 12:05:05 +0700

    fixed pythonpath for linux

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/installs/lib/python"
-PATH="$PATH:$SCRIPT_DIR/usd/installs/bin"
+PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/install/lib/python"
+PATH="$PATH:$SCRIPT_DIR/usd/install/bin"
 
 python "$SCRIPT_DIR/src/main.py"
