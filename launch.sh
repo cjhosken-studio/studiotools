0d8d04e5203144ec6b6a720dcb561c52e6da9c09
Christopher Hosken <hoskenchristopher@gmail.com>
2025-12-29 11:02:12 +0700

    changed export in ci

--- launch.sh
+++ launch.sh
@@ -12,7 +12,7 @@ ls $SCRIPT_DIR/usd
 ls $SCRIPT_DIR/usd/install
 
 
-PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/install/lib/python"
-PATH="$PATH:$SCRIPT_DIR/usd/install/bin"
+export PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/install/lib/python"
+export PATH="$PATH:$SCRIPT_DIR/usd/install/bin"
 
 python "$SCRIPT_DIR/src/main.py"
