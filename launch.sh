a06c5f29ff9b669a2f1cba867b24366026f8f7fb
Christopher Hosken <hoskenchristopher@gmail.com>
2025-12-29 10:01:20 +0700

    update ci

--- launch.sh
+++ launch.sh
@@ -3,6 +3,15 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo $SCRIPT_DIR
+
+ls $SCRIPT_DIR
+
+ls $SCRIPT_DIR/usd
+
+ls $SCRIPT_DIR/usd/install
+
+
 PYTHONPATH="$PYTHONPATH:$SCRIPT_DIR/usd/install/lib/python"
 PATH="$PATH:$SCRIPT_DIR/usd/install/bin"
 
